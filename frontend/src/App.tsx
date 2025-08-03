ec99de9e2c5c4cefc1d1bac00e7e4ce1889469ef
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-03 14:43:16 -0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import FileUploader from "./components/FileUploader";
-import ChatInterface from "./components/ChatInterface";
+import React from 'react';
+import Chatbot from './components/Chatbot';
+import FileUploader from './components/FileUploader';
 
 export default function App() {
-  const [csvInfo, setCsvInfo] = React.useState<any>(null);
-
   return (
-    <div className="p-4 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Petro Agent</h1>
-      <FileUploader onDetect={setCsvInfo} />
-      {csvInfo && (
-        <pre className="my-4 p-2 bg-gray-100 rounded">
-          {JSON.stringify(csvInfo, null, 2)}
-        </pre>
-      )}
-      <ChatInterface />
+    <div className="h-screen flex">
+      <div className="w-1/3 p-4 border-r">
+        <FileUploader />
+      </div>
+      <div className="flex-1 p-4">
+        <Chatbot />
+      </div>
     </div>
   );
 }
