e407e14dc4c7c2e7eac35df7e35a4c45852623bc
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-03 14:53:09 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
 app = FastAPI()
-
-class HelloRequest(BaseModel):
-    name: str
-
-@app.get("/")
-def root():
-    return {"message": "Backend up"}
-
-@app.post("/hello")
-def hello(req: HelloRequest):
-    return {"message": f"Hello, {req.name}!"}
+@app.get("/health")
+def health():
+    return {"status":"ok"}
