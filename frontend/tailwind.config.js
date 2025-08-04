fa1256a4a6ca09ed25f6b09ddcd867abe698ea99
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-03 20:41:21 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}","./public/index.html"],
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
   theme: { extend: {} },
-  plugins: []
+  plugins: [],
 }
