bfb25fcac2622e97156a0ff9fc948f194040e355
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-07 19:50:38 -0700

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: { extend: {} },
+  plugins: [],
+};
