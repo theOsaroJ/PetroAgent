05e210b48ea2556fe655dc31130ddc3adc01d26a
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-07 19:54:36 -0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
