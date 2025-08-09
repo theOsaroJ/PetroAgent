a0b8d324ff23628847d10ee82b0ada9f92178746
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-08 19:59:40 -0700

    Create init.py

---
+++ ml_service/trainers/init.py
@@ -0,0 +1,5 @@
+from .neural_net import train_neural_net
+from .random_forest import train_random_forest
+from .xgboost_model import train_xgboost
+from .gp import train_gp
+from .tab_transformer import train_tab_transformer
