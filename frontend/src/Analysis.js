7ac68074d704fba299c72fec442783720b00910e
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-08 19:54:54 -0700

    Delete frontend/src/Analysis.js

--- frontend/src/Analysis.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Analysis() {
-  return (
-    <div>
-      <h2 className="font-semibold mb-1">Analysis & Plots</h2>
-      <p className="text-gray-600">(Plots will appear here...)</p>
-    </div>
-  );
-}
