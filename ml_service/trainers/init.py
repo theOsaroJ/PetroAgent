b4d41b3555d759eee4c5bfa225ce42c7ffaa1e4c
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-08 22:58:38 -0700

    Delete ml_service/trainers/init.py

--- ml_service/trainers/init.py
+++
@@ -1,5 +0,0 @@
-from .neural_net import train_neural_net
-from .random_forest import train_random_forest
-from .xgboost_model import train_xgboost
-from .gp import train_gp
-from .tab_transformer import train_tab_transformer
