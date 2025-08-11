4fcd3c8c3aaec4aac280eb3df687a18167ab2168
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-10 18:33:02 -0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 5173 },
-  build: { outDir: 'dist' }
-})
+  build: { outDir: "dist" }
+});
