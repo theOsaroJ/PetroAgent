a733607e5ee4f197dc545fcb85d2e19f8ddfa5a4
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-08-11 19:26:53 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html","./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      colors: { brand: { 500: "#0ea5e9", 600: "#0284c7" } }
-    }
-  },
-  plugins: []
-}
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
